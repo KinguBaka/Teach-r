e4d16683de70ea82a2f7ea390c9681c25af3aa61
Tarik Aboulhouda <tarikab92@yahoo.fr>
2022-06-15 17:41:58 +0200

    création card + state search

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import Card from "./Card";
 
 function HomePage() {
+  const [search, setSearch] = useState("");
+
   return (
     <div className="HomePage">
       <div className="HomePage-Search">
         <h1>En quelle matiére avez-vous besoin d'aide ?</h1>
         <div className="SearchBar">
-          <input type="text" placeholder="SVT, piano, anglais, math..." />{" "}
+          <input
+            type="text"
+            placeholder="SVT, piano, anglais, math..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
           <FontAwesomeIcon
             className="search-icon"
             icon={faMagnifyingGlass}
           ></FontAwesomeIcon>
         </div>
-        <h1>Les Teach'rs d'Anglais qui pourraient vous correspondre</h1>
+        <h1>Les Teach'rs d'{search} qui pourraient vous correspondre</h1>
       </div>
-      <div className="HomePage-Card">Card ici</div>
+      <div className="HomePage-Card">
+        <Card />
+      </div>
       <div className="HomePage-Button">
         <a href="" className="Btn-gris">
           PRÉCEDENT
