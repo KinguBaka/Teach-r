2158dfea0ede71f628d6fa5befcf181f2c45ff75
Tarik Aboulhouda <tarikab92@yahoo.fr>
2022-06-17 14:36:51 +0200

    maj responsive form

--- src/components/Form.js
+++ src/components/Form.js
@@ -104,9 +104,6 @@ function FormPage() {
               type="password"
               placeholder="xxxxxxxx"
             />
-            <p className="text-danger">
-              <ErrorMessage name="password1" />
-            </p>
             <div className="cnt-icon">
               {eyes ? (
                 <FontAwesomeIcon
@@ -122,6 +119,9 @@ function FormPage() {
                 />
               )}
             </div>
+            <p className="text-danger">
+              <ErrorMessage name="password1" />
+            </p>
           </div>
           <div className="labelInput">
             <label htmlFor="password2">Confirmation mot de passe</label>
@@ -161,80 +161,6 @@ function FormPage() {
           </div>
         </Form>
       </Formik>
-
-      {/* <form className="form">
-        <div className="labelInput">
-          <label htmlFor="firstName">Prénom</label>
-          <input type="text" name="firstName" placeholder="Ex. John" />
-        </div>
-        <div className="labelInput">
-          <label htmlFor="lastName">Nom</label>
-          <input type="text" name="lastName" placeholder="Ex. DUPONT" />
-        </div>
-        <div className="labelInput">
-          <label htmlFor="email">E-mail</label>
-          <input type="email" name="email" placeholder="mail@mail.com" />
-        </div>
-        <div className="labelInput">
-          <label htmlFor="tel">No. Téléphone portable</label>
-          <input type="tel" name="tel" placeholder="Ex. 06 XX XX XX XX" />
-        </div>
-        <div className="labelInput">
-          <label htmlFor="password1">Mot de passe</label>
-          <input
-            className="mdp1"
-            name="password1"
-            type="password"
-            placeholder="xxxxxxxx"
-          />
-          <div className="cnt-icon">
-            {eyes ? (
-              <FontAwesomeIcon
-                className="icon"
-                icon={faEye}
-                onClick={afficherMdp}
-              />
-            ) : (
-              <FontAwesomeIcon
-                className="icon"
-                icon={faEyeSlash}
-                onClick={afficherMdp}
-              />
-            )}
-          </div>
-        </div>
-        <div className="labelInput">
-          <label htmlFor="password2">Confirmation mot de passe</label>
-          <input
-            className="mdp2"
-            name="password2"
-            type="password"
-            placeholder="xxxxxxxx"
-          />
-          <div className="cnt-icon">
-            {eyes2 ? (
-              <FontAwesomeIcon
-                className="icon"
-                icon={faEye}
-                onClick={afficherMdp2}
-              />
-            ) : (
-              <FontAwesomeIcon
-                className="icon"
-                icon={faEyeSlash}
-                onClick={afficherMdp2}
-              />
-            )}
-          </div>
-        </div>
-
-        <div className="form-Button">
-          <a href="" className="Btn-gris">
-            PRÉCEDENT
-          </a>
-          <input type="submit" value="SUIVANT" className="Btn-blanc" />
-        </div>
-      </form> */}
     </div>
   );
 }
